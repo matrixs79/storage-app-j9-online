8a0deb9f536423627c350a9186c369c4b9df1480
Stefan Chalat <stefan.chalat@gmail.com>
2019-06-01 10:57:11 +0200

    initial commit

---
+++ src/main/java/akademia/StorageApp9OnlineApplication.java
@@ -0,0 +1,13 @@
+package akademia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageApp9OnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageApp9OnlineApplication.class, args);
+    }
+
+}
